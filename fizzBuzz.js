34764247eae4776a53f14923f99c9ab0da028aea
seyma <seyma.6ok@hotmail.com>
2024-01-30 11:50:43 +0300

    fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,25 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+
+
+readline.question('enter a number: ', (enteredNumber) => {
+    function fizzBuzz(number) {
+      if (number % 3 === 0 && number % 5 === 0) {
+        return "FizzBuzz";
+      } else if (number % 3 === 0) {
+        return "Fizz";
+      } else if (number % 5 === 0) {
+        return "Buzz";
+      } else {
+        return number;
+      }
+    }
+  
+    let result = fizzBuzz(Number(enteredNumber));
+    console.log(result);
+  
+    readline.close();
+  });
